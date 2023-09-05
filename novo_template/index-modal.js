77279fe6e5b6d6a2416ac8c7516a45419182fd90
Aline Souza <aln.aline@hotmail.com>
2023-09-05 15:11:43 -0300

    adicao dos modais para ong

--- novo_template/index-modal.js
+++ novo_template/index-modal.js
@@ -1,144 +1,150 @@
-// const header = document.querySelector('.barra_topo');
-// const cornerThreshold = 0.3; // 30% dos cantos
+const header = document.querySelector('.barra_topo');
+const cornerThreshold = 0.3; // 30% dos cantos
 
-// window.addEventListener('mousemove', (event) => {
-//     const mouseX = event.clientX;
-//     const mouseY = event.clientY;
-//     const screenWidth = window.innerWidth;
-//     const screenHeight = window.innerHeight;
+window.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-//     if (
-//         (mouseX <= screenWidth * cornerThreshold || mouseX >= screenWidth * (1 - cornerThreshold))
-//     ) {
-//         header.style.top = '0';
-//     } else {
-//         header.style.top = '-50px';
-//     }
-// });
+    if (
+        (mouseX <= screenWidth * cornerThreshold || mouseX >= screenWidth * (1 - cornerThreshold))
+    ) {
+        header.style.top = '0';
+    } else {
+        header.style.top = '-50px';
+    }
+});
 
-// // Referência para o balão de recepção e seus controles
-// const balao = document.querySelector("#balao_recepcao");
-// const btnFechar = document.querySelector(".balao_btn");
-// const elemento = document.querySelector("#recepcao");
+// Referência para o balão de recepção e seus controles
+const balao = document.querySelector("#balao_recepcao");
+const btnFechar = document.querySelector(".balao_btn");
+const elemento = document.querySelector("#recepcao");
 
-// // Função para mostrar o balão de recepção
-// function showBalao() {
-//     // Obtém a posição do elemento #recepcao
-//     const rect = elemento.getBoundingClientRect();
+// Função para mostrar o balão de recepção
+function showBalao() {
+    // Obtém a posição do elemento #recepcao
+    const rect = elemento.getBoundingClientRect();
     
-//     // Define a posição do balão baseado na posição de #recepcao
-//     balao.style.left = (rect.left - balao.offsetWidth / 1.9) + "px";
-//     balao.style.top = (rect.top - balao.offsetHeight - 30) + "px";
+    // Define a posição do balão baseado na posição de #recepcao
+    balao.style.left = (rect.left - balao.offsetWidth / 1.9) + "px";
+    balao.style.top = (rect.top - balao.offsetHeight - 30) + "px";
     
-//     // Mostra o balão
-//     balao.classList.add('visible');
-// }
+    // Mostra o balão
+    balao.classList.add('visible');
+}
 
-// // Exibe o balão no carregamento da página
-// showBalao();
+// Exibe o balão no carregamento da página
+showBalao();
 
-// // Evento para fechar o balão
-// btnFechar.addEventListener('click', () => balao.classList.remove('visible'));
+// Evento para fechar o balão
+btnFechar.addEventListener('click', () => balao.classList.remove('visible'));
 
-// // Evento para mostrar o balão ao clicar no #recepcao
-// elemento.addEventListener('click', showBalao);
+// Evento para mostrar o balão ao clicar no #recepcao
+elemento.addEventListener('click', showBalao);
 
-// // Funções para manipular eventos de mouse para os balões
-// function handleMouseEvents(balaoElement) {
-//     return {
-//         over: function() {
-//             // Posiciona e mostra o balão ao passar o mouse
-//             const rect = this.getBoundingClientRect();
-//             balaoElement.style.left = rect.left + "px";
-//             balaoElement.style.top = (rect.top - balaoElement.offsetHeight) + "px";
-//             balaoElement.classList.add('visible');
-//         },
-//         out: function() {
-//             // Esconde o balão ao remover o mouse do elemento
-//             balaoElement.classList.remove('visible');
-//         }
-//     };
-// }
+// Funções para manipular eventos de mouse para os balões
+function handleMouseEvents(balaoElement) {
+    return {
+        over: function() {
+            // Posiciona e mostra o balão ao passar o mouse
+            const rect = this.getBoundingClientRect();
+            balaoElement.style.left = rect.left + "px";
+            balaoElement.style.top = (rect.top - balaoElement.offsetHeight) + "px";
+            balaoElement.classList.add('visible');
+        },
+        out: function() {
+            // Esconde o balão ao remover o mouse do elemento
+            balaoElement.classList.remove('visible');
+        }
+    };
+}
 
-// // Mapeamento dos elementos e seus respectivos balões
-// const balaoElements = {
-//     "#racao": "#balao_racao",
-//     "#roupinhas":"#balao_origem",
-// };
-// console.log(balaoElements);
+// Mapeamento dos elementos e seus respectivos balões
+const balaoElements = {
+    "#racao": "#balao_racao",
+    "#roupinhas":"#balao_origem",
+};
+console.log(balaoElements);
 
-// // Adiciona eventos para cada elemento mapeado e seu respectivo balão
-// for (let key in balaoElements) {
-//     const element = document.querySelector(key);
-//     const events = handleMouseEvents(document.querySelector(balaoElements[key]));
-//     element.addEventListener('mouseover', events.over);
-//     element.addEventListener('mouseout', events.out);
-// }
+// Adiciona eventos para cada elemento mapeado e seu respectivo balão
+for (let key in balaoElements) {
+    const element = document.querySelector(key);
+    const events = handleMouseEvents(document.querySelector(balaoElements[key]));
+    element.addEventListener('mouseover', events.over);
+    element.addEventListener('mouseout', events.out);
+}
 
-// // Função gerenciadora de modais (abrir e fechar)
-// function manageModal(modalSelector, overlaySelector) {
-//     const modal = document.querySelector(modalSelector);
-//     const overlay = document.querySelector(overlaySelector);
-//     const closeModalButton = modal.querySelector(".modalClose");
+// Função gerenciadora de modais (abrir e fechar)
+function manageModal(modalSelector, overlaySelector) {
+    const modal = document.querySelector(modalSelector);
+    const overlay = document.querySelector(overlaySelector);
+    const closeModalButton = modal.querySelector(".modalClose");
 
-//     return {
-//         open: function() {
-//             // Mostra o modal e o overlay
-//             modal.style.display = "block";
-//             overlay.style.display = "block";
-//         },
-//         close: function() {
-//             // Esconde o modal e o overlay
-//             modal.style.display = "none";
-//             overlay.style.display = "none";
-//         },
-//         init: function(triggerSelector) {
-//             // Inicializa os eventos para mostrar e esconder o modal
-//             document.querySelectorAll(triggerSelector).forEach(element => {
-//                 element.addEventListener('click', this.open);
-//             });
-//             closeModalButton && closeModalButton.addEventListener('click', this.close);
-//             overlay.addEventListener('click', this.close);
-//         }
-//     };
-// }
+    return {
+        open: function() {
+            // Mostra o modal e o overlay
+            modal.style.display = "block";
+            overlay.style.display = "block";
+        },
+        close: function() {
+            // Esconde o modal e o overlay
+            modal.style.display = "none";
+            overlay.style.display = "none";
+        },
+        init: function(triggerSelector) {
+            // Inicializa os eventos para mostrar e esconder o modal
+            document.querySelectorAll(triggerSelector).forEach(element => {
+                element.addEventListener('click', this.open);
+            });
+            closeModalButton && closeModalButton.addEventListener('click', this.close);
+            overlay.addEventListener('click', this.close);
+        }
+    };
+}
 
-// // Configuração do modal de origem
-// const modalOrigem = manageModal("#modal_origem", "#modalOverlay");
-// modalOrigem.init("#roupinhas");
-// // Configuração do modal de ração
-// const modalRacao = manageModal("#modal_pesq_alimentacao", "#modalOverlay");
-// modalRacao.init("#racao");
+// Configuração do modal de origem
+const modalOrigem = manageModal("#modal_origem", "#modalOverlay");
+modalOrigem.init("#roupinhas");
+// Configuração do modal de ração
+const modalRacao = manageModal("#modal_pesq_alimentacao", "#modalOverlay");
+modalRacao.init("#racao");
 
-// // Configuração do modal de adoção
-// const modalAdotante = manageModal("#modal_adotante", "#modalOverlay");
-// modalAdotante.init("#espera");
+// Configuração do modal de adoção
+const modalAdotante = manageModal("#modal_adotante", "#modalOverlay");
+modalAdotante.init("#espera");
 
-// // Configuração do modal de não-adotante
-// const modalNaoAdotante = manageModal("#modal_nao_adotante", "#modalOverlay");
-// modalNaoAdotante.init("#maca_2");
+const modalOngs = manageModal("#modal_ongs", "#modalOverlay");
+modalOngs.init("#gaiola_3");
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const idList = [
-//         "cachorro_colo", "carinho_chao", "atendente_adocao", "gaiola_3", "gaiola_2", "gaiola_1", "gaiola_vazia", "vet", "groomer", "dog_gym", "menina_cachorro", "recep_infocao", "gaiolas", "casinha", "brincando_bolinha", "quadro_3", "quadro_2", "quadro_1", "recepcao", "roupinhas", "almofada", "pct_racao", "racao", "corgi", "passeador", "espera", /* Adicione todos os IDs aqui */
-//     ];
+// Configuração do modal de não-adotante
+const modalNaoAdotante = manageModal("#modal_nao_adotante", "#modalOverlay");
+modalNaoAdotante.init("#vet");
 
-//     idList.forEach(id => {
-//         const image = document.getElementById(id);
-//         if (image) {
-//             // Adicionar evento de passar o mouse
-//             image.addEventListener("mouseenter", function() {
-//                 image.style.filter = "grayscale(100%)";
-//             });
+const modalDivulgueOngs = manageModal("#modal_divulgue_ongs", "#modalOverlay");
+modalDivulgueOngs.init("#recep_infocao");
 
-//             // Adicionar evento de retirar o mouse
-//             image.addEventListener("mouseleave", function() {
+document.addEventListener("DOMContentLoaded", function() {
+    const idList = [
+        "cachorro_colo", "carinho_chao", "atendente_adocao", "gaiola_3", "gaiola_2", "gaiola_1", "gaiola_vazia", "vet", "groomer", "dog_gym", "menina_cachorro", "recep_infocao", "gaiolas", "casinha", "brincando_bolinha", "quadro_3", "quadro_2", "quadro_1", "recepcao", "roupinhas", "almofada", "pct_racao", "racao", "corgi", "passeador", "espera", /* Adicione todos os IDs aqui */
+    ];
+
+    idList.forEach(id => {
+        const image = document.getElementById(id);
+        if (image) {
+            // Adicionar evento de passar o mouse
+            image.addEventListener("mouseenter", function() {
+                image.style.filter = "grayscale(100%)";
+            });
+
+            // Adicionar evento de retirar o mouse
+            image.addEventListener("mouseleave", function() {
                 
-//                 image.style.filter = "none";
-//             });
-//         }
-//     });
-// });
+                image.style.filter = "none";
+            });
+        }
+    });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     const mobileElements = document.querySelectorAll('.mobile');
